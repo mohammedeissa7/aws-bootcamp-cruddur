53eb2c704723f795d09c82edf56962e01ac66d9e
mohammed eissa7 <smme012345@gmail.com>
2024-10-26 15:24:36 +0000

    xray installing

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -13,9 +13,15 @@ from services.message_groups import *
 from services.messages import *
 from services.create_message import *
 from services.show_activity import *
+#xray -------------------------------
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
-# Honeycomb 
+# Honeycomb ---------------------------- 
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
